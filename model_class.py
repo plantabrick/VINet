e32a045cfe113551c044d2e6ee2850a5c816e419
gudonghao <2585341750@qq.com>
2019-10-30 22:07:53 +0800

    Change to a testable version and add test data

---
+++ model_class.py
@@ -0,0 +1,138 @@
+'''ResNet-18 Image classfication for cifar-10 with PyTorch 
+
+Author 'Sun-qian'.
+
+'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from util import resize
+
+class ResidualBlock(nn.Module):
+    def __init__(self, inchannel, outchannel, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.left = nn.Sequential(
+            nn.Conv2d(inchannel, outchannel, kernel_size=3, stride=stride, padding=1, bias=True),
+            nn.BatchNorm2d(outchannel,momentum=0.05),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(outchannel, outchannel, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(outchannel,momentum=0.05)
+        )
+        self.shortcut = nn.Sequential()
+        if stride != 1 or inchannel != outchannel:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(inchannel, outchannel, kernel_size=1, stride=stride, bias=True),
+                nn.BatchNorm2d(outchannel,momentum=0.05)
+            )
+
+    def forward(self, x):
+        out = self.left(x)
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, ResidualBlock):
+        super(ResNet, self).__init__()
+        self.inchannel = 64
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1, bias=True),
+            nn.BatchNorm2d(64,momentum=0.05),
+            nn.ReLU(),
+        )
+        self.mid = False
+        self.layer1 = self.make_layer(ResidualBlock, 32,  2, stride=1)
+        self.layer2 = self.make_layer(ResidualBlock, 64, 2, stride=2)
+        self.layer3 = self.make_layer(ResidualBlock, 96, 2, stride=2)
+        self.layer4 = self.make_layer(ResidualBlock, 128, 2, stride=2)
+        self.layer5 = self.make_layer(ResidualBlock, 196, 2, stride=2)
+        self.fc0 = nn.Linear(3136, 64)
+        self.fc1 = nn.Linear(64, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, 1)
+
+    def make_layer(self, block, channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)   #strides=[1,1]
+        layers = []
+        for stride in strides:
+            layers.append(block(self.inchannel, channels, stride))
+            self.inchannel = channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x, cover_mask):
+        if self.mid:
+            x,mid_list = self.cover(x,cover_mask,self.mid)
+        else:
+            x = self.cover(x, cover_mask)
+        out = self.conv1(x)
+        out = self.cover(out, cover_mask)
+        out = self.layer1(out)
+        out = self.cover(out, cover_mask)
+        out = self.layer2(out)
+        out = self.cover(out, cover_mask)
+        out = self.layer3(out)
+        # out = self.cover(out, cover_mask)
+        out = self.layer4(out)
+        # out = self.cover(out, cover_mask)
+        out = self.layer5(out)
+        out = out.view(out.size(0), -1)
+        # print out.size()
+        out = F.relu(self.fc0(out))
+        out = F.relu(self.fc1(out)+out)
+        out = F.relu(self.fc2(out)+out)
+        out = self.fc3(out)
+        if self.mid:
+            return out,mid_list
+        else:
+            return out
+
+    def cover(self,fea,cover_mask,mid=False):
+        if mid:
+            mid_list = []
+        b,c,h,w = fea.size()
+        cover_resize = resize(cover_mask,[h,w]).expand(b, c, h, w)
+        fea_max = F.max_pool2d(fea, (h, w), (h, w), padding=0).expand(b, c, h, w)
+        fea_min = -F.max_pool2d(-fea, (h, w), (h, w), padding=0).expand(b, c, h, w)
+        fea = (fea-fea_min)/(fea_max-fea_min+1e-8)
+        if mid:
+            mid_list.append(fea*1)
+        noise = torch.rand(fea.size()).cuda()
+        if mid:
+            mid_list.append(noise*1)
+            mid_list.append(noise*(1-cover_resize))
+        fea = fea*cover_resize+noise*(1-cover_resize)
+        if mid:
+            mid_list.append(fea*1)
+        fea = fea*(fea_max-fea_min+1e-8)+fea_min
+
+        if mid:
+            mid_list.append(fea*1)
+            return fea,mid_list
+        else:
+            return fea
+
+    def mid_on(self):
+        self.mid = True
+
+
+
+
+def CLASS_NET():
+
+    return ResNet(ResidualBlock)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,97 @@
+# coding=utf-8
+import os
+import torch
+import torch.nn as nn
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from model_class import CLASS_NET
+from model_cover import COVER_NET
+# import util
+import numpy as np
+import cv2
+import shutil
+import torch.nn.functional as F
+import math
+import random
+
+class_net = CLASS_NET()
+class_net.cuda().eval()
+
+cover_net = COVER_NET()
+cover_net.cuda().eval()
+
+ckpt_cover_path = "./ckpt_cover.pkl"
+cover_net.load_state_dict(torch.load(ckpt_cover_path))
+ckpt_class_path = "./ckpt_class.pkl"
+class_net.load_state_dict(torch.load(ckpt_class_path))
+
+batch = 1
+criterion = nn.CrossEntropyLoss()
+torch.set_grad_enabled(False)
+
+data_path = './demo_data/'
+transform = transforms.Compose([transforms.ToTensor()])
+train_set = datasets.ImageFolder(data_path, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch, shuffle=False, num_workers=2)
+
+dataiter = iter(train_loader)
+lenth = len(train_set)
+print 'img_num', lenth
+out_path = './view/'
+
+if os.path.exists(out_path):
+    shutil.rmtree(out_path)
+
+save_num = 0
+error_num = 0
+break_flag = 0
+area_add = 0
+batch_num = 0
+for images0, labels in dataiter:
+    if break_flag:
+        break
+    batch_num += 1
+    images0 = images0.cuda()
+    labels0 = labels.cpu().data.numpy()
+    # print labels0
+    images = images0 * 1
+    labels = labels.float().cuda()
+    cover_mask = cover_net(images)
+    class_out = class_net(images, cover_mask)
+    class_out = torch.mean(class_out, dim=1)
+    loss_class = (class_out - labels)
+    loss_class = (loss_class * loss_class).mean()
+    loss_cover = torch.mean(cover_mask)
+    area_add += loss_cover.cpu().data.numpy()
+
+    class_out = class_out.clamp(0, 5)
+    error_b = (class_out - labels).abs()
+    error_b += 0.5
+    error_b = error_b.floor().sum().cpu().data.numpy()
+    error_num += error_b
+
+    loss_class_c = loss_class.cpu().data.numpy()
+    loss_cover_c = loss_cover.cpu().data.numpy()
+
+    print "            loss_class:", loss_class_c, "   loss_cover:", loss_cover_c
+    batch_max = images.size(0)
+    for b in range(batch_max):
+        image_one = images0[b:b + 1, :, :, :]
+        cover_one = cover_mask[b:b + 1, :, :, :]
+        # overlay_one = image_one*1
+        # overlay_one[:,2,:,:] = overlay_one[:,2,:,:]*(1-cover_one)
+        image_one = util.torch2numpy(image_one * 255)
+        cover_one = np.squeeze(util.torch2numpy(cover_one * 255))
+        # overlay_one = util.torch2numpy(overlay_one*255)
+        overlay_one = image_one * 1
+        overlay_one[:, :, 2] = cv2.add(overlay_one[:, :, 2], cover_one)
+        save_path = out_path + str(labels0[b]) + '/'
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        cv2.imwrite(save_path + str(save_num) + '_a.jpg', image_one)
+        cv2.imwrite(save_path + str(save_num) + '_b.jpg', overlay_one)
+        cv2.imwrite(save_path + str(save_num) + '_c.jpg', cover_one)
+        save_num += 1
+
+print "error:", float(error_num) / float(save_num)
+print "area:", area_add / float(batch_num)
